6cbc58bd9c6215c5fdbfc7b75f06f2fd52133ff9
artigaspe_desktop <artigaspe@esat-alumni.com>
2022-12-10 19:44:48 +0100

    Added more functionality to Login button. Usage of SharedPreferences properly done

--- proyecto/App/Code/app/src/main/java/com/example/dsbosses/LoginActivity.kt
+++ proyecto/App/Code/app/src/main/java/com/example/dsbosses/LoginActivity.kt
@@ -1,11 +1,92 @@
 package com.example.dsbosses
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val userField: EditText = findViewById<EditText>(R.id.edtUser)
+        val passwordField: EditText = findViewById<EditText>(R.id.edtPassword)
+        val loginButton: Button = findViewById<Button>(R.id.btnLogin)
+        val rememberCheckBox: CheckBox = findViewById<CheckBox>(R.id.chkBoxRemember)
+
+        val sharedPreferences = getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+        val savedUser = sharedPreferences.getString("user", "").toString()
+        val savedPassword = sharedPreferences.getString("password", "").toString()
+        val userAlreadyRegistered = sharedPreferences.getBoolean("userRegistered", false)
+
+        if (userAlreadyRegistered){
+            userField.setText(savedUser)
+        }
+
+        /**
+         * Adds functionality to the LOGIN button.
+         * If user or password field is empty an alert is shown to the user.
+         */
+        loginButton.setOnClickListener{
+            if (userField.text.isEmpty() || passwordField.text.isEmpty()) {
+                alertEmptyFields()
+            }else {
+                val userString = userField.text.toString()
+                val passwordString = passwordField.text.toString()
+                if(rememberCheckBox.isChecked) {
+                    registerUser(sharedPreferences, userString, passwordString)
+                    Utils.startNewActivity(this, TitlesActivity())
+                }else{
+                    if(checkIfUserExists(userString, passwordString, savedUser, savedPassword)) {
+                        Utils.startNewActivity(this, TitlesActivity())
+                    }else{
+                        alertWrongCredentials()
+                    }
+                }
+
+            }
+        }
+    }
+
+    private fun alertEmptyFields() {
+        val alert = AlertDialog.Builder(this)
+        alert.setTitle("WATCH OUT!")
+        alert.setMessage("Please fill the fields with your user and password")
+        alert.setPositiveButton(
+            "Ok"
+        ) { dialog, which -> }
+        alert.show()
+    }
+
+    private fun alertWrongCredentials() {
+        val alert = AlertDialog.Builder(this)
+        alert.setTitle("WRONG CREDENTIALS!")
+        alert.setMessage("Error login. Incorrect user or password")
+        alert.setPositiveButton(
+            "Ok"
+        ) { dialog, which -> }
+        alert.show()
+    }
+
+    private fun registerUser(sharedPreferences: SharedPreferences, newUser: String, newPassword: String) {
+        val edit = sharedPreferences.edit()
+        edit.putString("user", newUser)
+        edit.putString("password", newPassword)
+        edit.putBoolean("userRegistered", true)
+        edit.commit()
+    }
+
+    private fun checkIfUserExists(user: String, password: String, savedUser: String, savedPassword: String): Boolean {
+        return ((user == savedUser && password == savedPassword) ||
+                (user == "wllop" && password == "MasterOfUniverse"))
     }
 }
--- proyecto/App/Code/app/src/main/java/com/example/dsbosses/Utils.kt
+++ proyecto/App/Code/app/src/main/java/com/example/dsbosses/Utils.kt
@@ -1,4 +1,13 @@
 package com.example.dsbosses
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+
 object Utils {
+
+    fun startNewActivity(currentContext: Context, newActivity: Activity) {
+        val intent = Intent(currentContext, newActivity::class.java)
+        currentContext.startActivity(intent)
+    }
 }
